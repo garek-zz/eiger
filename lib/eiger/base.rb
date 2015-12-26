1ef8b3d22d259038efe239578def9f060aeab1e5
Marek Gajewski <marek.gajewski@redefine.pl>
2015-12-26 17:25:37 +0100

    string and regexp routes

--- lib/eiger/base.rb
+++ lib/eiger/base.rb
@@ -7,19 +7,87 @@ module Eiger
       end
 
       def route(request_method, path, &block)
+        return unless valid_path?(path)
+
         method_name = method_name(request_method, path)
         define_singleton_method(method_name, &block)
+        add_route(request_method, path)
+      end
+
+      def add_route(request_method, path)
+        @routes ||= {}
+        @routes[request_method] ||= []
+        @routes[request_method] << path
       end
 
       def method_name(request_method, path)
         "#{request_method} #{path}".to_sym
       end
 
-      def call(env)
-        request     = Rack::Request.new(env)
-        method_name = method_name(request.request_method, request.fullpath)
+      def get_route(request_method, path)
+        @routes[request_method].find do |route|
+          if route.is_a?(String)
+            match_method(route, path)
+          elsif route.is_a?(Regexp)
+            path =~ route
+          end
+        end
+      end
 
-        if respond_to?(method_name)
+      def match_method(route, path)
+        path_segments   = path.split('/')
+        route_segments  = route.split('/')
+        method          = route
+
+        return nil if path_segments.size != route_segments.size
+
+        route_segments.each_with_index do |segment, i|
+          if (segment.empty? || segment =~ /w+/) && segment != path_segments[i]
+            method = nil
+          end
+        end
+
+        method
+      end
+
+      def match_params(route, path)
+        return {} if route.is_a? Regexp
+
+        path_segments   = path.split('/')
+        route_segments  = route.split('/')
+        params          = {}
+
+        route_segments.each_with_index do |segment, i|
+          params[segment[1..-1].to_sym] = path_segments[i] if segment =~ /:\w+/
+        end
+
+        params
+      end
+
+      def valid_path?(path)
+        if path.is_a? String
+          path.split('/').none? do |seg|
+            seg.match(/:\w+|\w+/) && seg.match(/:\w+|\w+/)[0] != seg
+          end
+        elsif path.is_a? Regexp
+          true
+        else
+          fail TypeError
+        end
+      end
+
+      def params
+        @params ||= match_params(@route, @path)
+      end
+
+      def call(env)
+        @request = Rack::Request.new(env)
+        @path    = @request.fullpath
+        @route   = get_route(@request.request_method, @path)
+
+        if @route
+          method_name = method_name(@request.request_method, @route)
+
           [200, {}, [send(method_name)]]
         else
           [404, {}, ['Page not found']]
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -3,19 +3,88 @@ require 'spec_helper'
 describe Eiger::Base, type: :controller do
   describe 'GET' do
     class TestApp < Eiger::Base
-      get('/') { 'Hello World' }
+      get('/') {}
+      get('/name/:name') { params[:name] }
+      get(%r{^\/regexp$}) {}
     end
 
     let(:request) { Rack::MockRequest.new(TestApp) }
 
-    it 'returns 200 status' do
-      response = request.get('/')
-      expect(response.status).to eq 200
+    context 'String route' do
+      it 'returns 200 status' do
+        response = request.get('/')
+        expect(response.status).to eq 200
+      end
+
+      it 'returns 200 status with params' do
+        response = request.get('/name/hello')
+        expect(response.status).to eq 200
+      end
+
+      it 'returns params :name' do
+        response = request.get('/name/hello')
+        expect(response.body).to eq 'hello'
+      end
+
+      context 'path whithout param' do
+        it 'returns 404 status' do
+          response = request.get('/name/')
+          expect(response.status).to eq 404
+        end
+      end
+
+      it 'returns 404 status' do
+        response = request.get('/test')
+        expect(response.status).to eq 404
+      end
     end
 
-    it 'returns 404 status' do
-      response = request.get('/test')
-      expect(response.status).to eq 404
+    context 'Regexp route' do
+      it 'returns 200 status' do
+        response = request.get('/regexp')
+        expect(response.status).to eq 200
+      end
+
+      it 'returns 404 status' do
+        response = request.get('/Regexp')
+        expect(response.status).to eq 404
+      end
+    end
+  end
+
+  describe '#get_route' do
+    let(:base) { Eiger::Base }
+
+    context 'String path' do
+      let(:path) { '/name/:name' }
+
+      before do
+        base.add_route('GET', path)
+      end
+
+      it 'returns route' do
+        expect(base.get_route('GET', '/name/hello')).to eq path
+      end
+
+      it 'returns nil' do
+        expect(base.get_route('GET', '/name/')).to eq nil
+      end
+    end
+
+    context 'Regexp path' do
+      let(:path) { %r{^\/(name|test)$} }
+
+      before do
+        base.add_route('GET', path)
+      end
+
+      it 'returns route' do
+        expect(base.get_route('GET', '/name')).to eq path
+      end
+
+      it 'returns nil' do
+        expect(base.get_route('GET', '/names')).to eq nil
+      end
     end
   end
 end
